4f6324c5433f20677ad4aa5a45e736a57c3a7fe9
Autismayi <93170741+Autismayi@users.noreply.github.com>
2025-09-14 02:03:41 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -11,7 +11,7 @@ export default siteConfig({
   // basic site info (required)
   name: 'xiaoyi's blog',
   domain: 'www.stat200.net',
-  author: 'Xiaoyi Wang',
+  author: 'Xiaoyi Wang', 
 
   // open graph metadata (optional)
   //description: 'Example Next.js Notion Starter Kit Site',
